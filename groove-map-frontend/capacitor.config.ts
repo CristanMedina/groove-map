8ab32d7c63384b4594990be1519c9459a8b795e2
Cristan <crishighlights@gmail.com>
2025-10-10 18:43:54 -0700

    Inicializando Groove Map

---
+++ groove-map-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'groove-map-frontend',
+  webDir: 'www'
+};
+
+export default config;
