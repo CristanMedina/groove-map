d5d7f85925fc779e94b291640b177e9dae61405d
Cristan Medina <crishighlights@gmail.com>
2025-10-27 20:34:30 -0700

    Navbar implementado

--- groove-map-frontend/src/app/components/bottom-nav-bar/bottom-nav-bar.component.ts
+++ groove-map-frontend/src/app/components/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  mapOutline, map,
+  sparklesOutline, sparkles,
+  personOutline, person
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-bottom-nav-bar',
   templateUrl: './bottom-nav-bar.component.html',
   styleUrls: ['./bottom-nav-bar.component.scss'],
+  imports: [RouterLink, RouterLinkActive, IonIcon]
 })
 export class BottomNavBarComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      mapOutline,
+      map,
+      sparklesOutline,
+      sparkles,
+      personOutline,
+      person
+    });
+  }
 
   ngOnInit() {}
 
