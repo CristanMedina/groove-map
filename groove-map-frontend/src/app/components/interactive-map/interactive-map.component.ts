32afb0bebf548ffef1ac87efb55638a9755b2d90
Cristan Medina <crishighlights@gmail.com>
2025-10-28 21:29:42 -0700

    Intentando implementar el mapa

--- groove-map-frontend/src/app/components/interactive-map/interactive-map.component.ts
+++ groove-map-frontend/src/app/components/interactive-map/interactive-map.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-interactive-map',
   templateUrl: './interactive-map.component.html',
   styleUrls: ['./interactive-map.component.scss'],
+  imports: [IonContent],
 })
-export class InteractiveMapComponent  implements OnInit {
+export class InteractiveMapComponent implements AfterViewInit {
 
-  constructor() { }
+  private map!: L.Map;
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    this.initMap();
+  }
 
+  private initMap(): void {
+    const mapContainer = document.getElementById('map');
+    if (!mapContainer) {
+      console.warn("No se encontró el contenedor #map");
+      return;
+    }
+
+    this.map = L.map(mapContainer).setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
 }
--- groove-map-frontend/src/app/pages/map-page/map-page.component.ts
+++ groove-map-frontend/src/app/pages/map-page/map-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { InteractiveMapComponent } from "src/app/components/interactive-map/interactive-map.component";
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.scss'],
-  imports: [InteractiveMapComponent],
+  imports: [InteractiveMapComponent, IonContent],
 })
 export class MapPageComponent  implements OnInit {
 
